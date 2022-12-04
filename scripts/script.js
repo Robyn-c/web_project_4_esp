f0e84c2ab834782e929f5bf6f31008910785409f
SCuadra02 <scuadra02@gmail.com>
2022-12-03 23:17:23 -0300

    feat: add button first attempt

--- scripts/script.js
+++ scripts/script.js
@@ -54,3 +54,43 @@ heartButton.forEach((heart) => {
     heart.classList.toggle("card__heart_liked");
   });
 });
+
+const cardContainer = document.querySelector(".cards");
+
+function addCard(srcValue, titleValue) {
+  const cardTemplate = document.querySelector("#card").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").setAttribute(src, srcValue);
+  cardElement.querySelector(".card__title").textContent = titleValue;
+  cardContainer.append(cardElement);
+}
+
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+initialCards.forEach((card) => console.log(card.name));
+console.log(initialCards);
