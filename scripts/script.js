9a37fc6c84376c256946199e6687748a68f111a9
SCuadra02 <scuadra02@gmail.com>
2022-12-14 20:22:59 -0300

    fix: HTML errors

--- scripts/script.js
+++ scripts/script.js
@@ -219,7 +219,7 @@ const setEventListeners = (formElement) => {
 };
 
 const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll("#form"));
+  const formList = Array.from(document.querySelectorAll(".form"));
   formList.forEach((formElement) => {
     formElement.addEventListener("submit", function (evt) {
       evt.preventDefault();
